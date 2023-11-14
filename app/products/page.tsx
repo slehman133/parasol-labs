176700487bc6113e42737132f0989cd53709bd3f
slehman133 <43526258+slehman133@users.noreply.github.com>
2023-11-14 14:03:01 -0800

    add description to product card

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -17,6 +17,7 @@ const getProducts = async () => {
                 amount
               }
             }
+            description
             images(first: 1){
               edges{
                 node{
@@ -44,13 +45,14 @@ const ProductsPage = async () => {
           {products.map((e: any) => {
             const item = e.node
             const id = item.id.slice(-13)
+            const description = item.description
             const image = item.images.edges[0]?.node.transformedSrc
             const altText = item.images.edges[0]?.node.altText
             const price = item.priceRange.minVariantPrice.amount
 
             return <ProductCard
               key={item.handle} handle={item.handle} image={image} title={item.title}
-              altText={altText} price={price} />
+              altText={altText} price={price} description={description} />
           })}
         </div>
       </div>
