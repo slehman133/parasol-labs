854239dc6136a5b0ef68a2d1e8a5d1d03c10c501
slehman133 <43526258+slehman133@users.noreply.github.com>
2023-12-09 13:49:13 -0800

    fix provider order issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,11 +34,11 @@ export default async function RootLayout({
         <Provider session={session}>
           <CartProvider>
             <Navbar />
+            {children}
           </CartProvider>
         </Provider>
-        {children}
         <Footer></Footer>
       </body>
-    </html>
+    </html >
   );
 }
