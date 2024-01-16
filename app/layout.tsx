a5d84cbc0f9411f4dae139e69d20e62a100a7860
kaedenc <kaedenc714@nevada.unr.edu>
2024-01-16 08:22:09 -0800

    Integrated NextUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
-import Navbar from "./components/navigation/navbar/Navbar";
-import Footer from "./components/navigation/footer/Footer";
 import "./globals.css";
-import type { Metadata } from "next";
-import { Cookie, Exo_2, Inter } from "next/font/google";
-import { CartProvider } from "./context/CartContext";
 import Provider from "./context/ClientProvider";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/utils/authOptions";
-import { Exo } from "next/font/google";
 import GoogleAnalytics from "./components/googleanalytics/GoogleAnalytics";
 import Google from "next-auth/providers/google";
 import CookieBanner from "./components/cookiebanner/CookieBanner";
+import { NextProviders } from "./NextProviders";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+import type { Metadata } from "next";
+import { Cookie, Exo_2, Inter } from "next/font/google";
+import { CartProvider } from "./context/CartContext";
+
 const exo = Exo_2({
   subsets: ["latin"],
 });
@@ -28,21 +27,21 @@ export default async function RootLayout({
 }) {
   const session = await getServerSession(authOptions);
   return (
-    <html lang="en" className="backgroundMain ">
+    <html lang="en">
       <GoogleAnalytics GA_MEASUREMENT_ID="G-2QHVKTJB8F" />
       <head>
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico"></link>
       </head>
       <body className={inter.className}>
-        <Provider session={session}>
-          <CartProvider>
-            <Navbar />
-            {children}
-            <CookieBanner/>
-          </CartProvider>
-        </Provider>
-        <Footer></Footer>
+        <NextProviders>
+          <Provider session={session}>
+            <CartProvider>
+              {children}
+              <CookieBanner />
+            </CartProvider>
+          </Provider>
+        </NextProviders>
       </body>
-    </html >
+    </html>
   );
 }
