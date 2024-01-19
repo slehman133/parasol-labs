0b6cb3f47bfa913b375b73ad4f0227a4eb7b2ef6
kaedenc <kaedenc714@nevada.unr.edu>
2024-01-19 15:28:04 -0800

    Privacy Page added and edited cookie bar. TODO: fix cookie bar's resizing

--- app/components/cookiebanner/CookieBanner.tsx
+++ app/components/cookiebanner/CookieBanner.tsx
@@ -4,7 +4,7 @@ import "./Cookie.css";
 import React from "react";
 import { getLocalStorage, setLocalStorage } from "@/lib/storageHelper";
 import { useState, useEffect } from "react";
-import { Button } from "@nextui-org/react";
+import { Button, Divider, Link } from "@nextui-org/react";
 
 export default function CookieBanner() {
   const [cookieConsent, setCookieConsent] = useState();
@@ -30,43 +30,46 @@ export default function CookieBanner() {
 
   return (
     <div
-      className={`cookie-consent-banner justify-between ${
+      className={`cookie-consent-banner ${
         cookieConsent != null ? "hidden" : "flex"
       }`}
     >
-      <div className="flex w-96">
-
-        <p className="mt-2.5 ml-10">
-          we like cookies.
-        </p>
+      <div className="flex justify-between gap-5 p-1">
+        <header className="mt-auto font-bold">We value your <Link href="/privacy" color="success" underline="hover">Privacy</Link>.</header>
+        <p className="text-xs mt-auto">We use cookies to enhance your browsing experience by serving personalized ads or content, and analyze our traffic. By clicking "Accept All", you consent to the use of cookies.</p>
+        <Link href="/privacy" color="success" underline="always" className="text-xs mt-0 mb-5">Privacy Policy</Link>
       </div>
-      <div className="flex gap-4 mx-5">
+      <div>
+        <Divider className="bg-black" orientation="vertical"/>
+      </div>
+      <div className="flex gap-4 mx-5 ">
         {/* Argument is not assignable, yet it assigns on click properly and adjusts gcs tag??? 
           Only fix is if const [cookieConsent, setCookieConsent] = useState(false);
           Buuut that assigns our user's cookie consent to false, and it automatically hides since it has a value. cool.*/}
+
         <Button
-          className="mt-1 mb-1 text-red-500 font-medium"
-          color="danger"
-          variant="ghost"
-          onClick={() => setCookieConsent(false)}
-        >
-          Decline
-        </Button>
-        <Button
-          className="mt-1 mb-1 text-orange-500 font-medium"
-          color="warning"
-          variant="ghost"
+          className="my-auto text-green-500 font-medium"
+          variant="light"
           onClick={() => setCookieConsent(false)} //TODO - create a pop up that allows user to customize their cookie consent
         >
           Customize Cookies
         </Button>
         <Button
-          className="mt-1 mb-1 text-green-500 font-medium"
+          className="my-auto text-green-500 font-medium"
+          color="success"
+          variant="ghost"
+          onClick={() => setCookieConsent(false)}
+        >
+          Decline
+        </Button>
+
+        <Button
+          className="my-auto text-green-500 font-medium"
           color="success"
           variant="ghost"
           onClick={() => setCookieConsent(true)}
         >
-          Allow Cookies
+          Accept All
         </Button>
       </div>
     </div>
---
+++ app/privacy/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+const PrivacyPage = async () => {
+  return (
+    <>
+      <div className="leftSection justify-evenly">
+        <header className="text-3xl font-bold">Your Privacy matters.</header>
+        <header className="text-xl font-semibold">SECTIONS</header>
+      </div>
+
+      <div className="rightSection">you got </div>
+    </>
+  );
+};
+export default PrivacyPage;
