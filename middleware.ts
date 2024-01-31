8d8ac15e3536a0ad81401d76fcd6e392474e10f6
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-01-30 19:14:52 -0800

    disable middleware for test deploy

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
-// import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
-// export async function middleware(request: NextRequest) {
-//     const token = await getToken({ req: request })
-//     if (token?.role !== 'admin' || !token) {
-//         return NextResponse.redirect(new URL('/not-found', request.url))
-//     }
-// }
+export async function middleware(request: NextRequest) {
+    // const token = await getToken({ req: request })
+    // if (token?.role !== 'admin' || !token) {
+    //     return NextResponse.redirect(new URL('/not-found', request.url))
+    // }
+    console.log("ok")
+}
 
-// export const config = {
-//     matcher: [
-//         '/admin/:path*',
-//         '/api/admin/:path*'
-//     ]
-// }
+export const config = {
+    matcher: [
+        '/admin/:path*',
+        '/api/admin/:path*'
+    ]
+}
