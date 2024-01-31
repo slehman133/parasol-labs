7e08fabff4a9d4f3bf6a5900e704374aa37cff80
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-01-30 19:29:28 -0800

    nvm

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
+    const token = await getToken({ req: request })
     if (token?.role !== 'admin' || !token) {
         return NextResponse.redirect(new URL('/not-found', request.url))
     }
