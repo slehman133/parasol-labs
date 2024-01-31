ac32ddd26ab7ed49aef6126b82a18a81b5567dfb
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-01-30 19:25:23 -0800

    add secret to middleware

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,10 @@ import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
-    // const token = await getToken({ req: request })
-    // if (token?.role !== 'admin' || !token) {
-    //     return NextResponse.redirect(new URL('/not-found', request.url))
-    // }
-    console.log("ok")
+    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
+    if (token?.role !== 'admin' || !token) {
+        return NextResponse.redirect(new URL('/not-found', request.url))
+    }
 }
 
 export const config = {
