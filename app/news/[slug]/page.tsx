7930bb71ca6c99b8cea63fa41af61dc89a41774d
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-01-31 21:43:16 -0800

    some formating for news page

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,24 +1,43 @@
 const ArticlePage = async (props: any) => {
+    let key = 0
     const slug = props.params.slug
     const res = await fetch(`https://q7s7f1a1.api.sanity.io/v2021-06-07/data/query/local?query=*`,
         { cache: 'no-store' })
         .then(res => res.json())
     const data = res.result.filter((e: any) => e.slug?.current == slug)[0]
     // console.log(data)
+    const author = null
+    const bodyText = data.body.map((e: any) => e.children).flat().map((e: any) => e.text)
 
     const imageURL = data.mainImage ?
         `https://cdn.sanity.io/images/q7s7f1a1/local/${data.mainImage.asset._ref
             .replace("image-", "")
-            .replace("-webp", ".webp")}` : null
+            .replace("-webp", ".webp")
+            .replace("-jpeg", ".jpeg")
+        }` : null
 
     return (
         <>
-            <div className='m-20'>
-                <h1>{data.title}</h1>
-                {imageURL &&
-                    <img src={imageURL} alt="image" />
-                }
-                <p>{data.body[0].children[0].text}</p>
+            <div className="my-24 grid grid-cols-[20%_60%_20%]">
+                <div className='text-black col-start-2'>
+                    <h1 className="text-5xl font-bold">{data.title}</h1>
+                    {imageURL &&
+                        <div className="my-10">
+                            <img className='mx-auto' src={imageURL} alt="image" />
+                        </div>
+                    }
+                    <div>
+                        {bodyText.map((e: string) => {
+                            return (
+                                <>
+                                    <div key={`${++key}${new Date().getTime()}`} className="my-4">
+                                        <p className="text-xl">{e}</p>
+                                    </div>
+                                </>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </>
     )
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,10 +1,7 @@
-// 'use client'
-
 import React from 'react'
 import getPostMetadata from '../components/Blog/getPostMetadata';
 import PostPreview from '../components/Blog/PostPreview';
 import Link from "next/link"
-// import {useSession} from "next/auth"
 
 
 //developed, using integrated components, see component files for integrations
@@ -20,44 +17,23 @@ export async function generateMetadata() {
 
 const NewsPage = async () => {
     const res = await fetch(`https://q7s7f1a1.api.sanity.io/v2021-06-07/data/query/local?query=*`, { cache: 'no-store' }).then(res => res.json())
-    // const {data:session} = useSession();
-    // console.log(session)
-    // const postMetadata = getPostMetadata();
-    // const postPreviews = postMetadata.map((post) => (
-    //     <PostPreview key={post.slug} {...post} />
-    // ));
-    console.log(res.result)
+    // console.log(res.result)
 
     return (
         <>
-            {/* <div className="flex flex-col p-24"> */}
-
-
-
-            {/* <div className="flex border justify-between bg-slate-500 p-8">
-                    <h2 className='text-3xl'>NEWS</h2>
-                    {/* {session.isAdmin && */}
-            {/* <Link href="admin/blogPost/">
-                    <p className="btn btn-ghost bg-slate-100">
-                        <span className="text-2xl">New Post</span>
-                    </p>
-                </Link> */}
-            {/* } */}
-            {/* </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-3 p-8 gap-8">{postPreviews}</div> */}
-            {/* </div > */}
-            <div className='m-20'>
-                {res.result.map((e: any) => {
-                    return (
-                        <>
-                            <Link key={e.slug?.current} href={`/news/${e.slug?.current}`}>
-                                <div>
-                                    <h1>{e.title}</h1>
-                                </div>
-                            </Link>
-                        </>)
-                })}
+            <div className="my-24 grid grid-cols-[20%_60%_20%]">
+                <div className='col-start-2 mx-auto'>
+                    {res.result.map((e: any) => {
+                        return (
+                            <>
+                                <Link key={e.slug?.current} href={`/news/${e.slug?.current}`}>
+                                    <div className='border-2 border-black'>
+                                        <h1>{e.title}</h1>
+                                    </div>
+                                </Link>
+                            </>)
+                    })}
+                </div>
             </div>
         </>
 
