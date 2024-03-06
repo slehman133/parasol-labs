e8efd6b00789cba6c90fa65b21d28718266d62b6
KaedenC <kaedenc714@nevada.unr.edu>
2024-03-05 22:00:49 -0800

    About page done - styling webforms

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,71 +1,118 @@
 import React from 'react';
 import { Divider, Button, Link, Image } from '@nextui-org/react';
 import "./aboutpagestyles.css";
+import SendEmailForm from '../components/webforms/emailform';
 
 export default function About() {
   return (
     <>
       <div>
-        <section className='content grid grid-cols-2 grid-rows-1 gap-10 '>
-          <div className='w-full text-wrap text-left'>
-            <h1 className='font-bold text-6xl'>Our Mission Statement.</h1>
-            <Divider orientation='horizontal' className='my-5' />
-            <div className='grid grid-rows-2 gap-5'>
-              <div>
-                <p className='font-thin text-2xl'>Parasol Laboratories Incorporated is dedicated to pioneering transformative solutions that solve global health needs, empower marginalized communities, and target underserved markets.
-                  <br />
-                  <br />
-                  Every product we design, every decision we make, is anchored in creating a tangible, positive impact, with no compromise. Our responsibility extends beyond profit, as we reinvest in our community, ensuring equitable care and quality of life are forever at the forefront of our endeavors.
-                </p>
-              </div>
-              <div className=' justify-end inline-flex items-end h-1/6'>
-                <Link href="/about" color="foreground">
-                  <div className="grid grid-columns-2 grid-flow-col gap-4">
-                    <h1 className="font-bold text-2xl py-5">About us</h1>
-                    <svg id="right" className="arrow-right my-auto ">
-                      <path
-                        d="M0.5 9.35772H20.9956L14.2001 2.29941L16.4134 0L27 11L16.4134 22L14.2001 19.7006L20.9956 12.6423H0.5V9.35772Z"
-                        fill="#ffffff"
-                      ></path>
-                    </svg>
-                  </div>
-                </Link>
+        <section className='relative h-96 z-0'>
+          <Image
+            src='/images/msa_test 5log10 reduction.webp'
+            sizes='lg'
+            height="50%"
+            width="auto"
+            className='-z-10 absolute top-0'
+            radius='none'
+          />
+          <h1 className=' font-bold text-9xl text-center '>About <span className='text-amber-600'>Parasol</span></h1>
+          <Divider orientation='horizontal' className='w-1/2 bg-white mx-auto' />
+        </section>
+        <div>
+          <section className='content grid grid-cols-2 grid-rows-1 gap-10 mt-12 relative bg-background '>
+            <div className='w-full text-wrap text-left'>
+              <h1 className='font-bold text-7xl'>Our Mission Statement.</h1>
+              <Divider orientation='horizontal' className='my-5' />
+              <div className='grid grid-rows-2 gap-5'>
+                <div>
+                  <p className='font-light text-3xl'>Parasol Laboratories Incorporated is dedicated to pioneering transformative solutions that solve global health needs, empower marginalized communities, and target underserved markets.
+                    <br />
+                    <br />
+                    Every product we design, every decision we make, is anchored in creating a tangible, positive impact, with no compromise. Our responsibility extends beyond profit, as we reinvest in our community, ensuring equitable care and quality of life are forever at the forefront of our endeavors.
+                  </p>
+                </div>
+                <div className=' justify-end inline-flex items-end h-1/6'>
+                  <Button radius='none' color='warning' size='lg' className='my-auto w-2/5'>
+                    <Link href="/" color="foreground">
+                      <div className="grid grid-columns-2 grid-flow-col gap-1 h-auto w-auto items-end">
+                        <h1 className="font-bold text-2xl py-5">See our work</h1>
+                        <svg id="right" className="arrow-right my-auto ">
+                          <path
+                            d="M0.5 9.35772H20.9956L14.2001 2.29941L16.4134 0L27 11L16.4134 22L14.2001 19.7006L20.9956 12.6423H0.5V9.35772Z"
+                            fill="#ffffff"
+                          ></path>
+                        </svg>
+                      </div>
+                    </Link>
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='p-10 w-2/3 h-auto mx-auto'>
-            <div className='styled-box'>
+            <div className='p-10 w-2/3 h-auto mx-auto'>
               <Image
                 src="/images/logo.png"
                 alt='Parasol Laboratories Logo'
-                className='w-2/3 h-auto mx-auto top-0'
+                className='w-auto h-auto mx-auto top-0'
               />
             </div>
-          </div>
-        </section>
-        <section className='content grid grid-cols-[20%_80%] grid-rows-1 gap-10 bg-slate-300 text-black'>
-          <div className='p-10 w-2/3 h-auto mx-auto'>
-            <div className='styled-box'>
+          </section>
+          <section id='Operational' className='content grid grid-cols-[30%_70%] gap-10 bg-slate-300 text-black box-shadow'>
+            <div>
               <Image
-                src="/images/logo.png"
-                alt='Parasol Laboratories Logo'
-                className='w-2/3 h-auto mx-auto top-0'
+                src="/images/leica.webp"
+                alt='Image of a leica microscope.'
+                className='w-auto h-[100%] mx-auto top-0'
               />
             </div>
-          </div>
-          <div>
-            <h1 className='font-bold text-6xl'>
-              Operational Statement.
-            </h1>
-            <p>
-              Every day at Parasol Laboratories Incorporated, we operationalize our mission by engineering products with the end user’s best interest in mind. Our pricing models allow us to produce even more while giving back, ensuring that our innovations reach those who need them most. We champion environmental responsibility, sustainability, and neutrality in all our processes.
+            <div>
+              <h1 className='font-bold text-7xl text-left'>
+                Operational Statement.
+              </h1>
+              <Divider orientation='horizontal' className='my-5' />
+              <div className='grid grid-rows-[80%_20%] gap-10 content-start'>
+                <div className='text-left'>
+                  <p className='font-light text-3xl '>Every day at Parasol Laboratories Incorporated, we operationalize our mission by engineering products with the end user’s best interest in mind. Our pricing models allow us to produce even more while giving back, ensuring that our innovations reach those who need them most. We champion environmental responsibility, sustainability, and neutrality in all our processes.
+                    <br />
+                    <br />
+                    Our team, our greatest asset, is nurtured in an environment that values their well-being, rewards their expertise, and encourages continuous growth. Together, we work towards building a lasting legacy of positive change and unparalleled impact, internally and externally.
+                  </p>
+                </div>
+                <div className='justify-start inline-flex items-start w-4/5'>
+                  <Button color='success' radius='none' size='lg' className='my-auto w-1/3'>
+                    <Link href="/" color="foreground">
+                      <div className="grid grid-columns-2 grid-flow-col gap-1 h-auto w-auto items-end">
+                        <h1 className="font-bold text-2xl py-5 text-black">Meet our team</h1>
+                        <svg id="right" className="arrow-right my-auto ">
+                          <path
+                            d="M0.5 9.35772H20.9956L14.2001 2.29941L16.4134 0L27 11L16.4134 22L14.2001 19.7006L20.9956 12.6423H0.5V9.35772Z"
+                            fill="#000000"
+                          ></path>
+                        </svg>
+                      </div>
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className='content grid grid-cols-[30%_70%] gap-4'>
+            <div>
 
-              Our team, our greatest asset, is nurtured in an environment that values their well-being, rewards their expertise, and encourages continuous growth. Together, we work towards building a lasting legacy of positive change and unparalleled impact, internally and externally.
-            </p>
-
-          </div>
-
-        </section>
+              <h1 className='font-bold text-5xl text-left my-auto'>
+                Contact us.
+                <br />
+                <br />
+              </h1>
+              <p className='font-medium text-xl text-left'>
+                Fill out the form with an inquiry and we will reach out to you.
+              </p>
+            </div>
+            <div className='w-2/3 mx-auto'>
+              <SendEmailForm />
+            </div>
+          </section>
+        </div>
       </div>
     </>
   );
