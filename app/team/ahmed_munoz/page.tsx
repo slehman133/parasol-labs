6da3cade4bbf893afc2226f2713537628cd68b7b
1001_Whitehorn_Nicholas <pagelineestreu@gmail.com>
2024-03-07 11:04:13 -0800

    Format Fix on Team Member page

--- app/team/ahmed_munoz/page.tsx
+++ app/team/ahmed_munoz/page.tsx
@@ -13,16 +13,16 @@ const MemberPage = () => {
     return (
         <>
             <div className="flex md:p-48">
-                <div className="flex flex-col">
-                    <div className="flex-auto">
+                <div className="flex flex-col gap-4">
+                    <div className="flex-auto mx:auto">
                         <Image
                         src="/images/AhmedPic.jpg"
                         alt='Picture of Ahmed Munoz, founder of Parasol Labs'
-                        width = "1600"
-                        height = "1600"
+                        width = "600"
+                        height = "600"
                         />
                     </div>
-                    <div className="flex text-xl">
+                    <div className="flex flex-col text-xl gap-4">
                         <h1 className="text-3xl">Ahmed Munoz</h1>
                         <h2 className="">Founder and CEO</h2>
                         <p>Innovative and driven IT consultant with proven leadership experience in architecture, infrastructure, solutions delivery, IT Operations, enterprise security, project management, hyper-converged systems, vendor relations, and agile/waterfall development workflows. Recognized for working on the bleeding edge of technology to promote individual development, optimize business processes, increase profitability, boost productivity, and enhancing team cohesion while reducing overhead.Innovative and driven IT consultant with proven leadership experience in architecture, infrastructure, solutions delivery, IT Operations, enterprise security, project management, hyper-converged systems, vendor relations, and agile/waterfall development workflows. Recognized for working on the bleeding edge of technology to promote individual development, optimize business processes, increase profitability, boost productivity, and enhancing team cohesion while reducing overhead.</p>
