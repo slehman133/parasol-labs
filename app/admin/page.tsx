c60d00cf8af999caf57f9d2d2dbf097c801db6c0
kaedenc <kaedenc714@nevada.unr.edu>
2024-03-27 15:25:24 -0700

    Added general forms tables for both partnershipforms and generalforms - not complete

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import PartnershipFormTable from "../components/webforms/tables/partnershipformtable";
+import GeneralFormTable from "../components/webforms/tables/generalformtable";
 export async function generateMetadata() {
-    return {
-        title: "Admin - Parasol Labs",
-    };
+  return {
+    title: "Admin - Parasol Labs",
+  };
 }
 
 const AdminPage = () => {
-    return (
-        <div>AdminPage</div>
-    )
-}
+  return (
+    <div className="mx-auto my-auto">
+      {/* <PartnershipFormTable /> */}
+      <GeneralFormTable />
+    </div>
+  );
+};
 
-export default AdminPage
+export default AdminPage;
--- app/api/webforms/generalform/route.ts
+++ app/api/webforms/generalform/route.ts
@@ -29,3 +29,31 @@ export async function POST(req: Request) {
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
 }
+
+//Develop get method to get all the general forms
+export async function GET() {
+    try {
+        const forms = await prisma.contactForm.findMany();
+        return NextResponse.json(forms);
+    }
+    catch(e) {
+        console.error("ERROR: ", e);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
+//Develop a method to delete a general form
+export async function DELETE(req: Request) {
+    try {
+        const formData = await req.json();
+        await prisma.contactForm.delete({
+            where: {
+                id: formData.id
+            }
+        });
+        return NextResponse.json({ status: 200 });
+    }
+    catch(e) {
+        console.error("ERROR: ", e);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
