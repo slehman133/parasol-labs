4dd7a3c1537c889a377ac257bab8cbaf61b41bef
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-04-11 17:04:54 -0700

    couple fixes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import ProductDisplay from '../components/admin/ProductDisplay';
 import OrderDisplay from '../components/admin/OrderDisplay';
 import { getProducts } from '@/utils/storefront';
 import { getOrders } from '@/utils/shopifyAdmin';
-import { adminGetProducts, getOrders } from '@/utils/shopifyAdmin';
+// import { adminGetProducts, getOrders } from '@/utils/shopifyAdmin';
 import ProductSection from "../components/admin/ProductSection";
 
 export async function generateMetadata() {
@@ -28,30 +28,23 @@ const AdminPage = async () => {
                 <h1 className='text-4xl font-bold'>Messages</h1>
                 <div className='my-5'>
                     {/* <PartnershipFormTable /> */}
-                    {/* <GeneralFormTable /> */}
+                    <GeneralFormTable />
                 </div>
                 <h1 className='text-4xl font-bold'>Orders</h1>
                 <div className='my-5'>
                     <OrderDisplay orders={orders} />
                 </div>
-                </div>
                 <h1 className='text-4xl font-bold'>Analytics</h1>
                 <div className='my-5'>
-                    <iframe 
-                    width="1200" 
-                    height="900" 
-                    src="https://lookerstudio.google.com/embed/reporting/985175c2-a8e5-44a1-97df-dcbec3c2e0d8/page/9HNjD" 
-                    sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"/>
+                    <iframe
+                        width="1200"
+                        height="900"
+                        src="https://lookerstudio.google.com/embed/reporting/985175c2-a8e5-44a1-97df-dcbec3c2e0d8/page/9HNjD"
+                        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox" />
                 </div>
+                <div>
+                    <ProductSection products={products} />
                 </div>
-                {/* <div className="flex justify-between">
-                    <h1 className='text-4xl font-bold'>Products</h1>
-                    <button className="h-12 rounded-lg border-white border-2 font-bold px-5">Add Product</button>
-                </div>
-                <div className='my-5'>
-                    <ProductDisplay products={products} />
-                </div> */}
-                <ProductSection products={products} />
             </div>
         </>
 
