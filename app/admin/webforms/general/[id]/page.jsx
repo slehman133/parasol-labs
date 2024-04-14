a98df04a4104bab95181c262650dac4878ad3542
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-04-13 19:27:25 -0700

    scuffed fix

--- app/admin/webforms/general/[id]/page.jsx
+++ app/admin/webforms/general/[id]/page.jsx
@@ -6,24 +6,25 @@ import { SendEmail } from '@/app/api/email/contact';
 import EmailTemplate from '@/app/components/webforms/templates/emailtemplate';
 
 const statuses = [
-    {name: 'Active', value: 'Active'},
-    {name: 'Completed', value: 'Completed'},
+    { name: 'Active', value: 'Active' },
+    { name: 'Completed', value: 'Completed' },
 ]
 
-const GeneralWebformsPage = (props: { params: { id: string } }) => {
+// const GeneralWebformsPage = (props: { params: { id: string } }) => {
+const GeneralWebformsPage = (props) => {
     const variables = { formId: props.params.id }
     const [form, setForm] = useState([])
     //obtain the generalwebform that has the formId
-    const [htmlMessage, setHtmlMessage] = useState<string>('');
-    const [successMessage, setMessage] = useState<string>('');
-    const [status, setStatus] = useState<Selection>(new Set([]));
-    
+    const [htmlMessage, setHtmlMessage] = useState < string > ('');
+    const [successMessage, setMessage] = useState < string > ('');
+    const [status, setStatus] = useState < Selection > (new Set([]));
+
     const handleSendEmail = async () => {
-        if(htmlMessage === '' || form.status === 'Delivered') {
+        if (htmlMessage === '' || form.status === 'Delivered') {
             setMessage('Please enter a message before sending.');
             return;
         }
-        const error = await SendEmail({ to: form.email, subject: 'Parasol Laboratories: Engaging with Your Inquiry ', html: htmlMessage, name: form.name});
+        const error = await SendEmail({ to: form.email, subject: 'Parasol Laboratories: Engaging with Your Inquiry ', html: htmlMessage, name: form.name });
         if (!error) {
             console.error('Failed to send email');
         }
@@ -39,8 +40,8 @@ const GeneralWebformsPage = (props: { params: { id: string } }) => {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                ...form,
-                event: 'edit',
+                    ...form,
+                    event: 'edit',
                 })
             });
         }
@@ -72,24 +73,24 @@ const GeneralWebformsPage = (props: { params: { id: string } }) => {
     }, [])
     return (
         <>
-        <div className='flex flex-row gap-12 p-8 container'>
-            <div className='mx-auto'>
-                <h1 className='text-4xl font-semibold my-4'>Form: {form.id}</h1>
-                <Divider/>
-                <h2 className='my-4 font-medium text-xl'>Name: </h2>
-                <p>{form.name}</p>
-                <h2 className='my-4 font-medium text-xl'>Email: </h2>
-                <p>
-                    {form.email}
-                </p>
-                <h2 className='my-4 font-medium text-xl'>Message:</h2>
-                <p>{form.message}</p>
-            </div>
-            <div className='w-1/2'>
-                <div>
-                    <h1 className='mx-auto text-center text-3xl font-bold my-4'>Please Respond.</h1>
-                    <Divider/>
-                            <div className='my-12'>
+            <div className='flex flex-row gap-12 p-8 container'>
+                <div className='mx-auto'>
+                    <h1 className='text-4xl font-semibold my-4'>Form: {form.id}</h1>
+                    <Divider />
+                    <h2 className='my-4 font-medium text-xl'>Name: </h2>
+                    <p>{form.name}</p>
+                    <h2 className='my-4 font-medium text-xl'>Email: </h2>
+                    <p>
+                        {form.email}
+                    </p>
+                    <h2 className='my-4 font-medium text-xl'>Message:</h2>
+                    <p>{form.message}</p>
+                </div>
+                <div className='w-1/2'>
+                    <div>
+                        <h1 className='mx-auto text-center text-3xl font-bold my-4'>Please Respond.</h1>
+                        <Divider />
+                        <div className='my-12'>
                             <Select
                                 label="Status"
                                 variant='bordered'
@@ -100,14 +101,14 @@ const GeneralWebformsPage = (props: { params: { id: string } }) => {
                                     <SelectItem key={status.value} value={status.value}>
                                         {status.name}
                                     </SelectItem>
-                                ))} 
+                                ))}
                             </Select>
                         </div>
                         <div className='mt-12'>
                             <div className='flex flex-row justify-end'>
                                 <div className='flex flex-row gap-2'>
                                     <h2 className='my-auto'>Parasol Laboratories</h2>
-                                    <img src='/images/logo.png' alt='Parasol Labs Logo' width={30} height={30}/>
+                                    <img src='/images/logo.png' alt='Parasol Labs Logo' width={30} height={30} />
                                 </div>
 
                             </div>
@@ -135,11 +136,11 @@ const GeneralWebformsPage = (props: { params: { id: string } }) => {
                     </div>
                     <p className='mx-auto text-center'>{successMessage}</p>
                     <div className='flex justify-end'>
-            <Button variant='bordered' color='warning' onPress={() => window.history.back()}>Return</Button> 
-        </div>
-            </div>
+                        <Button variant='bordered' color='warning' onPress={() => window.history.back()}>Return</Button>
+                    </div>
+                </div>
 
-        </div>
+            </div>
 
 
         </>
