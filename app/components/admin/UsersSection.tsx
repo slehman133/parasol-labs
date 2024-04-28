cc9f377b69b404c56a9e78f0abf892bbc1673af7
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-04-28 13:16:55 -0700

    old table

--- app/components/admin/UsersSection.tsx
+++ app/components/admin/UsersSection.tsx
@@ -34,38 +34,57 @@ const UsersSection = () => {
 
   const classNames = useMemo(
     () => ({
-	@@ - 230, 110 + 51, 30 @@ const UsersSection = () => {
+      wrapper: ["max-h-[382px]"],
+      th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
+      td: [
+        // changing the rows border radius
+        // first
+        "group-data-[first=true]:first:before:rounded-none",
+        "group-data-[first=true]:last:before:rounded-none",
+        // middle
+        "group-data-[middle=true]:before:rounded-none",
+        // last
+        "group-data-[last=true]:first:before:rounded-none",
+        "group-data-[last=true]:last:before:rounded-none",
+      ],
+    }),
     []
   );
 
 
-return (
+  return (
     <>
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-      <ModalContent>
-        {(onClose) => (
-          <>
-            {loading ?
-              <div className='h-[25vh] grid grid-cols-3 grid-rows-3 place-items-center'>
-                <div className='col-start-2 row-start-2'>
-                  <Spinner className='mx-auto' />
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              {loading ?
+                <div className='h-[25vh] grid grid-cols-3 grid-rows-3 place-items-center'>
+                  <div className='col-start-2 row-start-2'>
+                    <Spinner className='mx-auto' />
+                  </div>
                 </div>
-              </div>
-              :
-              <>
-                <ModalHeader className="flex flex-col gap-1 items-center">
-                  Change Role
-                </ModalHeader>
-                <ModalBody className="gap-1 items-center">
-                  <form
-                    className='flex flex-col gap-3'>
-                    <div className='flex flex-col'>
-                      <label htmlFor="role">Choose new role for {userToChange.email}</label>
-                      <select name="role" id="role" onChange={(e) => setUserToChange({ ...userToChange, role: e.target.value })}>
-                        <option value="superadmin">Super Admin</option>
-                        <option value="admin">Admin</option>
-                        <option value="admin_products">Product Admin</option>
-	@@ -347,64 +88,101 @@ const UsersSection = () => {
+                :
+                <>
+                  <ModalHeader className="flex flex-col gap-1 items-center">
+                    Change Role
+                  </ModalHeader>
+                  <ModalBody className="gap-1 items-center">
+                    <form
+                      className='flex flex-col gap-3'>
+                      <div className='flex flex-col'>
+                        <label htmlFor="role">Choose new role for {userToChange.email}</label>
+                        <select name="role" id="role" value={userToChange.role} onChange={(e) => setUserToChange({ ...userToChange, role: e.target.value })}>
+                          <option value="superadmin">Super Admin</option>
+                          <option value="admin">Admin</option>
+                          <option value="admin_products">Product Admin</option>
+                          <option value="admin_forms">Form Admin</option>
+                          <option value="user">User</option>
+                        </select>
+                      </div>
+                    </form>
+                  </ModalBody>
+                  <ModalFooter>
                     <Button color="danger" variant="light" onPress={onClose}>
                       Close
                     </Button>
@@ -82,88 +101,88 @@ return (
                       Change Role
                     </Button>
                   </ModalFooter>
-                    </>
+                </>
               }
-                  </>
+            </>
 
           )}
-                </ModalContent>
-              </Modal >
+        </ModalContent>
+      </Modal >
 
-            <div className="flex flex-col justify-between">
-              <h1 className='text-4xl font-bold'>Users</h1>
-              <Table
-                classNames={classNames}
-                isCompact
-                removeWrapper>
-                <TableHeader>
-                  <TableColumn>
-                    User ID
-                  </TableColumn>
-                  <TableColumn>
-                    Email
-                  </TableColumn>
-                  <TableColumn>
-                    First Name
-                  </TableColumn>
-                  <TableColumn>
-                    Last Name
-                  </TableColumn>
-                  <TableColumn>
-                    Role
-                  </TableColumn>
-                </TableHeader>
+      <div className="flex flex-col justify-between">
+        <h1 className='text-4xl font-bold'>Users</h1>
+        <Table
+          classNames={classNames}
+          isCompact
+          removeWrapper>
+          <TableHeader>
+            <TableColumn>
+              User ID
+            </TableColumn>
+            <TableColumn>
+              Email
+            </TableColumn>
+            <TableColumn>
+              First Name
+            </TableColumn>
+            <TableColumn>
+              Last Name
+            </TableColumn>
+            <TableColumn>
+              Role
+            </TableColumn>
+          </TableHeader>
 
-                <TableBody>
-                  {users.map(({ id, email, firstName, lastName, role }: {
-                    id: string,
-                    email: string,
-                    firstName: string,
-                    lastName: string,
-                    role: string
-                  }) => {
-                    return (
-                      <TableRow
-                        className="cursor-pointer hover:border-2 hover:border-primary-500"
-                        key={id}
-                        onClick={() => {
-                          setUserToChange({ id, role, email })
-                          onOpen()
-                        }}>
-                        <TableCell>
-                          <div>
-                            {id}
-                          </div>
-                        </TableCell>
-                        <TableCell>
-                          <div>
-                            {email}
-                          </div>
-                        </TableCell>
-                        <TableCell>
-                          <div>
-                            {firstName}
-                          </div>
-                        </TableCell>
-                        <TableCell>
-                          <div>
-                            {lastName}
-                          </div>
-                        </TableCell>
-                        <TableCell>
-                          <div>
-                            {role}
-                          </div>
-                        </TableCell>
-                      </TableRow>
-                    )
-                  })}
-                </TableBody >
-              </Table >
+          <TableBody>
+            {users.map(({ id, email, firstName, lastName, role }: {
+              id: string,
+              email: string,
+              firstName: string,
+              lastName: string,
+              role: string
+            }) => {
+              return (
+                <TableRow
+                  className="cursor-pointer hover:border-2 hover:border-primary-500"
+                  key={id}
+                  onClick={() => {
+                    setUserToChange({ id, role, email })
+                    onOpen()
+                  }}>
+                  <TableCell>
+                    <div>
+                      {id}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div>
+                      {email}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div>
+                      {firstName}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div>
+                      {lastName}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div>
+                      {role}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody >
+        </Table >
 
-            </div>
-          </>
-        )
-        }
+      </div>
+    </>
+  )
+}
 
-        export default UsersSection
+export default UsersSection
