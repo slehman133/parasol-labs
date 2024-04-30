6ec36b7ba75bc4b7bc3d73b8fa935ca64e9941c1
KaedenC <kaedenc714@nevada.unr.edu>
2024-04-29 17:51:38 -0700

    team fix

--- app/team/cass_kirby/page.tsx
+++ app/team/cass_kirby/page.tsx
@@ -29,7 +29,6 @@ const MemberPage = () => {
                         <p className="md:text-xl text-medium">Passionate and visionary, Cass Kirby stands at the forefront of biomedical innovation as the CEO and co-founder of Parasol Labs. With a proven track record of leadership and a relentless pursuit of excellence, Cass drives Parasol Labs to push the boundaries of scientific discovery and redefine the future of healthcare. With a wealth of experience in the biomedical field, Cass brings a multifaceted skill set to the helm of Parasol Labs. From research and development to strategic planning and execution, Cass&apos;s expertise spans the entire spectrum of biomedical innovation. Cass is recognized for his unwavering commitment to driving positive change in healthcare. Through groundbreaking research initiatives and cutting-edge technologies, Cass and the Parasol Labs team are dedicated to tackling some of the most pressing health challenges facing humanity today. At Parasol Labs, Cass fosters a culture of collaboration and creativity, empowering a diverse team of scientists and researchers to push the boundaries of possibility. By championing innovation and embracing new ideas, Cass ensures that Parasol Labs remains at the forefront of biomedical research and development. Beyond the laboratory, Cass is a vocal advocate for the advancement of biomedical research and the importance of investing in scientific innovation. Through his leadership, Parasol Labs continues to shape the future of healthcare, making a meaningful impact on the lives of individuals around the world. In Cass Kirby, Parasol Labs has a visionary leader dedicated to driving positive change in healthcare through innovation, collaboration, and a relentless pursuit of excellence. With Cass at the helm, Parasol Labs is poised to continue revolutionizing the biomedical landscape and making a lasting impact on global health.</p>
                         <div className="flex flex-column py-4 gap-4">
                             <a
-                                className='light:invert'
                                 target="_blank"
                                 href="https://www.linkedin.com/in/cass-kirby/"
                             >
@@ -38,7 +37,8 @@ const MemberPage = () => {
                                     width="80"
                                     height="80"
                                     viewBox="0 0 24 24"
-                                    fill="#FFFFFF"
+                                    fill=""
+                                    className="dark:invert"
                                 >
                                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                                 </svg>
